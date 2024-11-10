0a2b7a1059a2f564c6a0041abeee6eb594df1158
Morggh <christophengals@gmail.com>
2024-11-10 08:41:56 -0500

    no more cardsetid

--- models/Flash_card_set.js
+++ models/Flash_card_set.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const flash_card_setSchema = new mongoose.Schema({
     userID: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    cardsetID: {type: Number, unique: true},
     name: String,
     topic: String
 })
