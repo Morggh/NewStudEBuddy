4215ff2be9b40262b516a941c38d3d9edd993c90
Caitlin Reardon <caitlinclarkreardon@gmail.com>
2024-11-10 02:45:03 -0800

    add ()

--- controllers/flashCards.js
+++ controllers/flashCards.js
@@ -25,7 +25,7 @@ exports.getCardSets = async (req, res) => {
     const userId = req.user._id;
 
     // Find flash card sets that belong to the logged-in user
-    const flashCardSets = await FlashCardSet.find({ userId }) || [];
+    const flashCardSets = (await FlashCardSet.find({ userId })) || [];
 
     // Render the Pug template and pass the data (ensure it's always an array)
     res.render('flashCardSets', { flashCardSets });
