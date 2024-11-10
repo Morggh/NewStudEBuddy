b6eb4ce01abef4dbc317d49c8161be987d03fdec
Caitlin Reardon <caitlinclarkreardon@gmail.com>
2024-11-09 22:35:57 -0800

    add validation

--- models/Flash_card.js
+++ models/Flash_card.js
@@ -2,7 +2,17 @@ const cardID = 0;
 const mongoose = require('mongoose');
 
 const flash_cardSchema = new mongoose.Schema({
-    cardID: {type: Number},
+    cardID: {type: Number,
+    required: [true, 'cardID is required'], 
+    validate: {
+      validator: function(value) {
+        return value !== 0; // Ensure cardID is not 0
+      },
+      message: 'cardID cannot be 0'
+    }
+
+
+    },
     //cardsetID: {type: mongoose.Schema.Types.ObjectId, ref: 'Flash_card_set'},
     term: String,
     content: String
