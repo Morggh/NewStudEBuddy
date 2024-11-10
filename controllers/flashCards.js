e9244ddf61429c4abdd6af15cbd403c690c3c8fb
Morggh <christophengals@gmail.com>
2024-11-10 02:07:27 -0500

    response added to flashcards

--- controllers/flashCards.js
+++ controllers/flashCards.js
@@ -1,11 +1,17 @@
 const Flashcard = require('../models/Flash_card');
+
 exports.renderForm = (req, res) => {
     res.render('flashCard_New', { title: 'User Input Form' });
   };
   
 exports.sendCard = async (req, res) => {
+  try {
     const flashcard = new Flashcard({
       term: req.body.term,
     });
     await flashcard.save();
+    res.status(201).send("Flashcard saved successfully!"); // Send success response
+  } catch (error) {
+    res.status(500).send("Error saving flashcard: " + error.message); // Send error response
+  }
 };
