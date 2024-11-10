ef3d1cd6231089745551d8d8b4b8ae1a5472be10
Caitlin Reardon <caitlinclarkreardon@gmail.com>
2024-11-09 22:41:10 -0800

    back to right model

--- models/Flash_card.js
+++ models/Flash_card.js
@@ -2,17 +2,7 @@ const cardID = 0;
 const mongoose = require('mongoose');
 
 const flash_cardSchema = new mongoose.Schema({
-    cardID: {type: Number,
-    required: [true, 'cardID is required'], 
-    validate: {
-      validator: function(value) {
-        return value !== 0; // Ensure cardID is not 0
-      },
-      message: 'cardID cannot be 0'
-    }
-
-
-    },
+    //cardID: {type: Number},
     //cardsetID: {type: mongoose.Schema.Types.ObjectId, ref: 'Flash_card_set'},
     term: String,
     content: String
