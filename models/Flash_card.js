e5c720e8995eaa58db373ab3c7f87e97894f3fa6
TheAiden02 <aidenhubbard02@gmail.com>
2024-11-10 00:42:29 -0500

    little things

--- models/Flash_card.js
+++ models/Flash_card.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const flash_cardSchema = new mongoose.Schema({
-    cardID: {type: Number, unique: true},
+    //cardID: {type: Number, unique: true},
     //cardsetID: {type: mongoose.Schema.Types.ObjectId, ref: 'Flash_card_set'},
     term: String,
     content: String
