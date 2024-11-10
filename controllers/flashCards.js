d1c63b870d8e4cf652cdcec672fec4e69bc9c15e
Caitlin Reardon <caitlinclarkreardon@gmail.com>
2024-11-09 19:42:16 -0800

    uncomment things

--- controllers/flashCards.js
+++ controllers/flashCards.js
@@ -1,20 +1,14 @@
-
-// const flashCards = require('../models/Flash_card'); //import model
-
-// //Function: add new item
-// async function addCard(term, content){
-//     const newCard = new flashCards({term: term , content: content});
-//     await newCard.save();
-// }
-
-// //Function: retrieve all from database
-
-// async function getWholeSet(){
-//     return await flashCards.find({});
-// }
-
-// module.exports = {
-//     addCard, getWholeSet, 
-// };
-
-
+exports.renderForm = (req, res) => {
+    res.render('/flashCard_New', { title: 'User Input Form' });
+  };
+  
+  // Handle form submission (you can expand this with your own logic)
+  exports.handleFormSubmit = (req, res) => {
+    const userInput = req.body.userInput; // Access the user input from the form
+  
+    // Here, you could process the user input (e.g., save to a database, etc.)
+    console.log('User Input:', userInput);
+  
+    // Redirect to a confirmation page or render a response
+    res.send(`<h1>Thank you for your input: ${userInput}</h1>`);
+  };
