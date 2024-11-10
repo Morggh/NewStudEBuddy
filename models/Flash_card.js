7b55f3556a6087ac55b939b7b0c9006905dca29e
Caitlin Reardon <caitlinclarkreardon@gmail.com>
2024-11-09 22:50:44 -0800

    this better work

--- models/Flash_card.js
+++ models/Flash_card.js
@@ -1,8 +1,5 @@
-const cardID = 0;
 const mongoose = require('mongoose');
-
 const flash_cardSchema = new mongoose.Schema({
-    //cardID: {type: Number},
     //cardsetID: {type: mongoose.Schema.Types.ObjectId, ref: 'Flash_card_set'},
     term: String,
     content: String
